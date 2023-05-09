6d70d4ab23ef6d701ea108a75e6b677939c78e76
Saravana16041999 <saravanakumar16041999@gmail.com>
2023-05-09 21:10:33 +0530

    master

---
+++ frontEnd/src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="header">
+        {/* should be a link */}
+        <div>
+          <a href="/">Shop New</a>
+        </div>
+      </header>
+      <div>Product List</div>
+    </div>
+  );
+}
+
+export default App;
